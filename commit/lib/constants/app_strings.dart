6869fb64f139dbc975a725023bb74dc05c2aa6d3
kashvigarg <kashvigarg02@gmail.com>
2023-05-08 21:32:21 +0530

    added app constants + dependencies

---
+++ commit/lib/constants/app_strings.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class AppStrings {
+  final String titleMsg = "Hi, welcome to my Accountability Tracker!";
+  final String subtitleMsg =
+      "This is a passion project that helps me commit to my goals (without the huss and fuss of posting the progress on socials and losing hours to the timeline)";
+}
